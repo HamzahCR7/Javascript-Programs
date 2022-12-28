9ca3836a9848a3f325a3ae31edd08dc971322ec8
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-28 19:55:08 +0530

    program to reverse an array

---
+++ reverseArray.js
@@ -0,0 +1,8 @@
+function reverseArr(arr){
+//   for(let i=arr.length-1;i>=0;i--){            a,5,4,3,2,1
+//       console.log(i+" "+arr[i]);
+//   }  
+console.log(arr.reverse());            // [ 'a', 5, 4, 3, 2, 1 ]          
+}
+let arr=[1,2,3,4,5,"a"];
+reverseArr(arr);
